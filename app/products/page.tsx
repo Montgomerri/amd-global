e27a5e1630bfa3f5ce588aa33bda263e8712f5d1
Dennis Nuamah <nuamahdennis923@gmail.com>
2026-04-12 17:33:00 +0000

    add revalidation to products page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,6 +2,8 @@
 import swell from "../../lib/swell";
 import ProductsClient from "./ProductsClient";
 
+export const revalidate = 60;
+
 export default async function ProductsPage() {
   const response = await swell.products.list({
     limit: 20,
